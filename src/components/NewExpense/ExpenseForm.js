c0716ea2b24b775c572172373ad0d4fcecfbf802
Ezeaku Ifechukwu Stanley <56836874+stanezeaku@users.noreply.github.com>
2022-10-27 12:56:14 +0100

    Handling form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,30 +1,93 @@
-import React from "react";
-import './ExpenseForm.css';
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-    return <form>
-        <div className="new-expense__controls">
-            <div className="new-expense__control">
-                <label>Title</label>
-                <input type='text'/>
-            </div>
-            <div className="new-expense__control">
-                <label>Amount</label>
-                <input type='number' min="0.01" step="0.01"/>
-                
+// // Combined state approach
+// const [userInput, setUserInput] = useState({
 
-            </div>
-            <div className="new-expense__control">
-                <label>Date</label>
-                <input type='date' min="2019-01-01" max="2022-12-31"/>
-            </div>
+//     enteredTitle: '',
+//     enteredAmount: '',
+//     enteredDate: ''
+
+// });
+
+  const titleChangeHandler = (event) => {
+
+    setEnteredTitle(event.target.value);
+
+// // if your state don't depend on previous state for update use below
+//     setUserInput({
+//         ...userInput,
+//         enteredTitle: event.target.value,
+//     });
+
+// // use this if your state update depends on the previous state snapshoot
+// setUserInput((prevState)=>{
+
+//     return {...prevState, enteredTitle: event.target.value};
+
+// });
+
+   
+  };
+
+  
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+  const submitHandler = (event)=>{
+    event.preventDefault();
+    const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+    };
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
-        <div className="new-expense__actions">
-            <button>Add Expense</button>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit" >Add Expense</button>
+      </div>
     </form>
-    
+  );
 };
 
 export default ExpenseForm;
